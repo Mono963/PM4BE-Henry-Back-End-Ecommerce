8f1e36907346698bc88ef55ae01797787d8eb4c2
Mono963 <ezeespa78@gmail.com>
2025-07-13 22:56:53 -0300

    Produccion

--- ecommerce-mono963/src/config/auth0.ts
+++ ecommerce-mono963/src/config/auth0.ts
@@ -1,7 +1,3 @@
-import { config as dotenvConfig } from 'dotenv';
-
-dotenvConfig({ path: '.env.development' });
-
 export const config = {
   authRequired: false,
   auth0Logout: true,
--- ecommerce-mono963/src/config/typeorm.ts
+++ ecommerce-mono963/src/config/typeorm.ts
@@ -1,23 +1,23 @@
 import { registerAs } from '@nestjs/config';
-import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-dotenvConfig({ path: '.env.development' });
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  url: process.env.SUPABASE_URL,
+  url: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
   autoLoadEntities: true,
   synchronize: false,
   logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/src/migrations/*{.ts,.js}'],
 };
+
 export default registerAs('typeorm', () => config);
 
 export const connectionSource = new DataSource(config as DataSourceOptions);
